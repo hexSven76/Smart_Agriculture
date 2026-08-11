db9d736d6853c32b9f8a9f945200df383c435f1b
Sven <sajad.var83@gmail.com>
2026-08-11 19:19:01 +0330

    extract_metrics.sh takes all sca/vec files with same name and filter and exports them to a single csv file.

---
+++ simulations/results/extract_metrics.sh
@@ -0,0 +1,16 @@
+for sca in *.sca; do
+    base="${sca%.sca}"
+    vec="${base}.vec"
+
+    if [ -f "$vec" ]; then
+        echo "Processing: $base"
+
+        opp_scavetool x \
+            -F CSV-R \
+            -f '(module =~ "AgricultureNetwork.sensor[*].app[0]" AND name =~ "packetSent*:count") OR (module =~ "AgricultureNetwork.server.app[0]" AND (name =~ "packetReceived*:count" OR name =~ "endToEndDelay*:vector" OR name =~ "throughput*:vector")) OR (module =~ "AgricultureNetwork.sensor[*].wlan[0].radio.energyConsumer" AND name =~ "powerConsumption:vector") OR (module =~ "AgricultureNetwork.gateway.wlan[0].radio.energyConsumer" AND name =~ "powerConsumption:vector")' \
+            "$sca" "$vec" \
+            -o "${base}_metrics.csv"
+    else
+        echo "WARNING: No matching .vec file for $sca"
+    fi
+done
