f3bde8cfb44bb0f05efbd890d62ec295250bedeb
Sven <sajad.var83@gmail.com>
2026-08-20 23:30:08 +0330

    updated .ini file with better config inheritance. also moved extract_metrics.sh out of results directory.

--- simulations/extract_metrics.sh
+++ simulations/extract_metrics.sh
@@ -1,6 +1,6 @@
-for sca in *.sca; do
-    base="${sca%.sca}"
-    vec="${base}.vec"
+for sca in results/*.sca; do
+    base="$(basename "${sca%.sca}")"
+    vec="results/${base}.vec"
 
     if [ -f "$vec" ]; then
         echo "Processing: $base"
@@ -9,7 +9,7 @@ for sca in *.sca; do
             -F CSV-R \
             -f '(module =~ "AgricultureNetwork.sensor[*].app[0]" AND name =~ "packetSent*:count") OR (module =~ "AgricultureNetwork.server.app[0]" AND (name =~ "packetReceived*:count" OR name =~ "packetReceived*:sum(packetBytes)" OR name =~ "endToEndDelay*:vector")) OR (module =~ "AgricultureNetwork.sensor[*].wlan[0].radio.energyConsumer" AND name =~ "powerConsumption:vector") OR (module =~ "AgricultureNetwork.gateway.wlan[0].radio.energyConsumer" AND name =~ "powerConsumption:vector")' \
             "$sca" "$vec" \
-            -o "${base}_metrics.csv"
+            -o "results/${base}_metrics.csv"
     else
         echo "WARNING: No matching .vec file for $sca"
     fi
